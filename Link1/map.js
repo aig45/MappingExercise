3d881dbe4ea2450a99ebade47d6621a9e38b8f30
aig45 <aig45@cornell.edu>
2025-02-06 00:58:58 -0500

    Add files via upload

---
+++ Link1/map.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWlnNDUiLCJhIjoiY20xMmd6bGlpMTNsMzJwcHFyZXE1MnkyMiJ9.GPJ6SmABq15ut_GkP5dzRQ';
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/aig45/cm6slmjn200h201qoh03bcfq0',
+    zoom: 12.1,
+    center: [-74, 40.725]
+});
