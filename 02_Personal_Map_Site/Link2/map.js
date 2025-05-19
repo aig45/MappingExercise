9918907c9d66bcb85e5da8f99d6e23307f9c1567
aig45 <aig45@cornell.edu>
2025-05-18 22:18:07 -0400

    Delete 02_Personal_Map_Site directory

--- 02_Personal_Map_Site/Link2/map.js
+++
@@ -1,7 +0,0 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWlnNDUiLCJhIjoiY20xMmd6bGlpMTNsMzJwcHFyZXE1MnkyMiJ9.GPJ6SmABq15ut_GkP5dzRQ';
-const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/aig45/cm6jjli5n004n01s7docea4sk',
-    zoom: 12.1,
-    center: [-0.10036, 51.51184]
-});
--- 02_Personal_Map_Site/README.md
+++
@@ -1,3 +0,0 @@
-#Repository for Alex-s-Epic-Website
-
-This repository was built for the Threads class, spring 2025
